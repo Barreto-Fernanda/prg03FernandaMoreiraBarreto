7d55984b292cd4d8ad6b339ff58311305d21a8bc
BarretoFernanda-dev <fernandamoreirabarreto82@gmail.com>
2026-09-10 21:36:44 -0300

    Tela de Login

--- prg03fernandamoreirabarreto/src/main/java/com/mycompany/prg03fernandamoreirabarreto/Prg03fernandamoreirabarreto.java
+++ prg03fernandamoreirabarreto/src/main/java/com/mycompany/prg03fernandamoreirabarreto/Prg03fernandamoreirabarreto.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.prg03fernandamoreirabarreto;
 
-/**
- *
- * @author ferna
- */
 public class Prg03fernandamoreirabarreto {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(() -> {
+            new br.com.ifba.login.view.TelaLogin().setVisible(true);
+        });
     }
 }
